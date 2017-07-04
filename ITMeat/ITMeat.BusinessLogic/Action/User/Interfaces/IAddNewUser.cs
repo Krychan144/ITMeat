aa2ce39947bc6bba892a22b56f04ccf8a54a5eb5
Unknown <krychan144@gmail.com>
2017-07-04 16:53:43 +0200

    fix ACTION

---
+++ ITMeat/ITMeat.BusinessLogic/Action/User/Interfaces/IAddNewUser.cs
@@ -0,0 +1,12 @@
+﻿using ITMeat.BusinessLogic.Action.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITMeat.BusinessLogic.Action.User.Interfaces
+{
+    public interface IAddNewUser : IAction
+    {
+        DataAccess.Models.User Invoke(Models.UserModel user);
+    }
+}
